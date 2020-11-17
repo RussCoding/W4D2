3175e92e5754ec47dd2972dc6444f275f26680aa
russelkamal <russelhkamal@gmail.com>
2020-11-17 13:57:54 -0500

    Initialized

--- Employee.rb
+++ Employee.rb
@@ -1,4 +1,18 @@
 class Employee
 
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
+    
+    def bonus(multiplier)
+        @bonus = salary * multiplier
+    end
 
+
+
+    attr_reader :name, :title, :boss
+    attr_accessor :salary
 end
--- Manager.rb
+++ Manager.rb
@@ -1,3 +1,8 @@
-class Manager
+require_relative "Employee"
+class Manager < Employee
+    def initialize(name, title, salary, boss, employees)
+        super(name, title, salary, boss)
+        @employees = employees
+    end
 
 end
